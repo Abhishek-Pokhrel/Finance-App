30efc629b757fbcfe540978b2ae4402212f0464d
Abhishek Pokhrel <abhishekpokhrel53@gmail.com>
2022-08-19 12:13:10 +0545

    Update script.js
    Fixed the total expenses calculator, added remove expenses functionality and dark mode.

--- script.js
+++ script.js
@@ -1,18 +1,12 @@
 let finalExpense = document.querySelector(".right-coln-expense-value");
-
 let expenseName = document.querySelector("#name");
-
 let expenseDate = document.querySelector("#date");
-
 let expenseAmount = document.querySelector("#amount");
-
 let addExpenseButton = document.querySelector(".add-expense");
-
+let delExpenseButton = document.querySelector(".del-expense");
+let modeButton = document.querySelector(".mode");
 let expenseEntities = document.querySelector(".bottom-entities");
-
 let finalColumn = document.querySelector(".final-col");
-
-
 
 addExpenseButton.addEventListener("click", function () {
   if (
@@ -35,7 +29,8 @@ addExpenseButton.addEventListener("click", function () {
     dateEnti.innerText = expenseDate.value;
 
     let amountEnti = document.createElement("div");
-    amountEnti.setAttribute("class", "amt");
+    amountEnti.setAttribute("class", "amt entiti");
+    
     amountEnti.innerText = expenseAmount.value;
 
     let delButton = document.createElement("button");
@@ -49,56 +44,63 @@ addExpenseButton.addEventListener("click", function () {
     currVal += parseInt(expenseAmount.value);
     finalExpense.innerText = currVal;
 
-    
-
     expenseName.value = "";
     expenseDate.value = "";
     expenseAmount.value = "";
   }
 
   let deleteButton = document.querySelectorAll(".delbutton");
-
   deleteButton.forEach((element) => {
     element.addEventListener("click", () => {
-    
-    finalExpense.innerText = parseInt(finalExpense.innerText) - parseInt(element.parentElement.children[2].innerHTML);
-     
-    var vls = document.querySelectorAll("#bottom-entities");
-
-    vls.forEach((element) => {
-      console.log(Array(element.innerText));
-    })
-    
-  
-
-    // element.parentElement.remove();
+      finalExpense.innerText = parseInt(finalExpense.innerText) - parseInt(element.parentElement.children[2].innerHTML);
+      element.parentElement.remove();
+      updateAmt();
     });
   });
 
-
-
-
-  // deleteButton.forEach((element) => {
-  //   DelCount = 0;
-  //   element.addEventListener("click", () => {
-  //     if (DelCount === 0) {
-  //       let prevVal = Number(finalExpense.textContent);
-  //       console.log(prevVal);
-  //       let delVal = Number(
-  //         element.parentElement.querySelector(".amt").textContent
-  //       );
-  //       let newVal = prevVal - delVal;
-  //       console.log(prevVal, delVal, newVal);
-  //       finalExpense.textContent = newVal;
-  //       DelCount++;
-  //     }
-
-  //     element.parentElement.remove();
-  //   });
-  // });
-
-
-
-
-
+  function updateAmt() {
+    let acc = 0;
+    let remValues = document.querySelectorAll('.amt');
+        remValues.forEach((element) => {
+          acc += parseInt(element.innerText)
+        });
+        finalExpense.innerText = acc;
+        console.log(acc);
+  }
 });
+
+delExpenseButton.addEventListener('click', function(){
+  document.querySelectorAll('#bottom-entities').forEach((element) => {
+    element.remove();
+  });
+  finalExpense.innerText = '0';
+});
+
+modeButton.addEventListener('click', function(){
+  if (modeButton.innerText === '☀️'){
+    modeButton.innerText = '🌙';
+    document.body.style.backgroundColor ='black';
+    expenseName.style.backgroundColor = 'black';
+    expenseName.style.color = 'white';
+    expenseDate.style.backgroundColor = 'black';
+    expenseDate.style.color = 'white';
+    expenseAmount.style.backgroundColor = 'black';
+    expenseAmount.style.color = 'white';
+    document.getElementById('title').style.color = 'white';
+    document.querySelector('.home').style.color = 'white';
+    document.querySelector('.left-coln-title').style.color = 'white';
+  }
+  else{
+    modeButton.innerText = '☀️';
+    document.body.style.backgroundColor ='white';
+    expenseName.style.backgroundColor = 'white';
+    expenseName.style.color = '#6C6C6C';
+    expenseDate.style.backgroundColor = 'white';
+    expenseDate.style.color = '#6C6C6C';
+    expenseAmount.style.backgroundColor = 'white';
+    expenseAmount.style.color = '#6C6C6C';
+    document.getElementById('title').style.color = '#6C6B6B';
+    document.querySelector('.home').style.color = '#00606D';
+    document.querySelector('.left-coln-title').style.color = '#6C6B6B';
+  }
+})
